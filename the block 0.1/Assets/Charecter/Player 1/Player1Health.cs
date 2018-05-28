132da14fa26fee48f80c1cdc957e962909f67e7b
Cpt-Panda <39674440+Cpt-Panda@users.noreply.github.com>
2018-05-28 05:00:08 +0100

    Healthbar added (glitchy)

---
+++ the block 0.1/Assets/Charecter/Player 1/Player1Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player1Health : MonoBehaviour {
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+    public Slider healthbar;
+
+	void Start ()
+    {
+        MaxHealth = 20f;
+        // Resets health to full on game load
+        CurrentHealth = MaxHealth;
+
+        healthbar.value = CalculateHealth();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(6);
+	}
+
+    void DealDamage(float damageValue)
+    {
+        // Deduct the damage dealt from the characters health
+        CurrentHealth - damageValue;
+        healthbar.value = CalculateHealth();    
+        // If the character is out of health, die!
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("Your Brown bread mate!");
+    }
+}
